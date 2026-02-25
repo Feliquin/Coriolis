c8749fcaabc7c23051e24a85d74da317c632dbaa
Feliquin <tommy@feliquin.de>
2026-02-25 13:10:52 +0100

    Quartz sync: Feb 25, 2026, 1:10 PM

--- quartz/components/Explorer.tsx
+++ quartz/components/Explorer.tsx
@@ -103,7 +103,7 @@ export default ((userOpts?: Partial<Options>) => {
           data-mobile={false}
           aria-expanded={true}
         >
-          <h2>{opts.title ?? i18n(cfg.locale).components.explorer.title}</h2>
+          <h3>{opts.title ?? i18n(cfg.locale).components.explorer.title}</h3>
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="14"
